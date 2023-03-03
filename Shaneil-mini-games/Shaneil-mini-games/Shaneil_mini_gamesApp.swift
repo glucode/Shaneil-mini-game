f39d66bf26b622cc8b528d97a3a2f88b448f687b
BronsonBru <104427833+BronsonBru@users.noreply.github.com>
2023-03-03 09:50:46 +0200

    project creation

---
+++ Shaneil-mini-games/Shaneil-mini-games/Shaneil_mini_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shaneil_mini_gamesApp.swift
+//  Shaneil-mini-games
+//
+//  Created by Bronson van den Broeck on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct Shaneil_mini_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
